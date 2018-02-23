182d4a910523053f1e7971d4dab75a9a47701280
TATJAVA_Andrei_Tsylko <mi113r@mail.ru>
2018-02-23 14:12:30 +0300

    Update index.js
    solved

--- src/index.js
+++ src/index.js
@@ -1,5 +1,32 @@
 // PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
+ if (currency > 10000) {
+        return {error: "You are rich, my friend! We don't have so much coins for exchange"}
+    }
+    if (currency <= 0) {
+        return {}
+    }
+
+    dic = {'H': 50, 'Q': 25, 'D': 10, 'N': 5, 'P': 1};
+    reminder = currency;
+    exchanged_money = {};
+    for (let key in dic) {
+        res = count(reminder, dic[key], key);
+
+        if (res[1][1] !== 0 && res[1][1] !== undefined) {
+            exchanged_money[res[1][0]] = res[1][1];
+        }
+        reminder = res[0];
+    }
+
+    return exchanged_money
+}
+function count(value, divivder, divider_name) {
+    reminder = value % divivder;
+    if (value / divivder > 0) {
+        amount = [divider_name, (value - reminder) / divivder];
+    } else {
+        amount = []
+    }
+    return [reminder, amount]
 }
